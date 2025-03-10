567d85afe0c4824209d506dee1be2760986ec975
Mihir Thakur <tamihir27@yahoo.com>
2025-03-09 21:55:10 -0700

    Updated endpoint

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM python:3.12.9-slim
 
 WORKDIR /app
 
-COPY requirements.txt ./
+COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 from src.main import ResumeAgent
 
 app = FastAPI()
 
+class ApplicationInfo(BaseModel):
+    resume: str
+    job_description: str
+
 @app.post("/tailor_resume")
-async def tailor_resume(resume: str, job_description: str):
+async def tailor_resume(app_info: ApplicationInfo):
     try:
+        resume = app_info.resume
+        job_description = app_info.job_description
+        if not resume:
+            raise HTTPException(status_code=400, detail="Resume is required")
+        if not job_description:
+            raise HTTPException(status_code=400, detail="Job Description is required")
         agent = ResumeAgent()
         response = agent.run(resume, job_description)
         return {"final_resume": response}
